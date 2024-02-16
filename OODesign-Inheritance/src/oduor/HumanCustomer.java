e42ab9b0acc781608681389bf010fdbc03da8eea
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-02-16 13:57:28 +0300

    Added comments

--- OODesign-Inheritance/src/oduor/HumanCustomer.java
+++ OODesign-Inheritance/src/oduor/HumanCustomer.java
@@ -5,7 +5,7 @@ public class HumanCustomer extends Customer{
     public HumanCustomer(String name) {
         super(name);
     }
-
+//overriddes the getName() method on Customer superclass
 String getName(){
         return  "--> I am " + getName()+ ", in need of customer service";
 }
