f3ea8f5d564dc8ceee17088c4d07ecc7daaabbbe
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-02-16 13:55:42 +0300

    Added comments

--- OODesign-Inheritance/src/oduor/BusinessCustomer.java
+++ OODesign-Inheritance/src/oduor/BusinessCustomer.java
@@ -2,10 +2,11 @@ package oduor;
 
 public class BusinessCustomer extends  Customer{
 
+    //constuctor
     public BusinessCustomer(String name) {
         super(name);
     }
-
+//overrides the getName() method on Customer
     String getName(){
         return "--> Hi, I am a business owner";
     }
